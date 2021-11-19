2f78bf96058e905abe713e9c2bbaf50fef9d2854
VitaliyZ3 <vitasiamen@gmai.com>
2021-11-19 12:49:13 +0200

    oooops

--- gym/admin.py
+++ gym/admin.py
@@ -6,3 +6,10 @@ from .models import Abonement, Trainer
 admin.site.register(Abonement)
 admin.site.register(Trainer)
 
+class MyAdmin(admin.ModelAdmin):
+    def log_addition(self, *args):
+            return
+    def log_change(self, *args):
+            return
+    def log_deletion(self, *args):
+            return
